1e3b4c0b804c5acffdcf4c9e4160021cad957c4a
DecliningLotus <declininglotus@gmail.com>
2021-09-23 10:23:01 +0100

    fix: remove npm download check since it's been too obstructive

--- index.js
+++ index.js
@@ -74,42 +74,36 @@ queue.drain(() => {
     precision: 2,
   })
 
-  // NPM downloads can be volatile and sometimes might error out a lot in a workflow.
-  // Hence we sometimes fluctuate in the millions.
-  if (
-    downloadsTotalBadge.replace(/\D/g, "") >
-    existingTotalDownload.message.replace(/\D/g, "")
-  ) {
-    const badgeMonth = {
-      schemaVersion: 1,
-      label: "downloads",
-      message: `${downloadsMonthBadge}/month`,
-      color: "brightgreen",
-    }
-    jsonfile.writeFileSync("./data/badgeMonth.json", badgeMonth)
+  console.log(`Month: ${badgeMonth}`)
+  console.log(`Year: ${badgeTotal}`)
 
-    const badgeTotal = {
-      schemaVersion: 1,
-      label: "downloads",
-      message: downloadsTotalBadge,
-      color: "brightgreen",
-    }
-    jsonfile.writeFileSync("./data/badgeTotal.json", badgeTotal)
-
-    // Reconstructs array back into object.
-    const sortedLastMonthPopular = Object.fromEntries(
-      // Flips from ascending order to descending. Deconstructs object to array for sort.
-      _.reverse(Object.entries(lastMonthPopular).sort(([, a], [, b]) => a - b))
-    )
-    const sortedTotalPopular = Object.fromEntries(
-      _.reverse(Object.entries(totalPopular).sort(([, a], [, b]) => a - b))
-    )
-    jsonfile.writeFileSync(
-      "./data/lastMonthPopular.json",
-      sortedLastMonthPopular
-    )
-    jsonfile.writeFileSync("./data/totalPopular.json", sortedTotalPopular)
+  const badgeMonth = {
+    schemaVersion: 1,
+    label: "downloads",
+    message: `${downloadsMonthBadge}/month`,
+    color: "brightgreen",
   }
+  jsonfile.writeFileSync("./data/badgeMonth.json", badgeMonth)
+
+  const badgeTotal = {
+    schemaVersion: 1,
+    label: "downloads",
+    message: downloadsTotalBadge,
+    color: "brightgreen",
+  }
+  jsonfile.writeFileSync("./data/badgeTotal.json", badgeTotal)
+
+  // Reconstructs array back into object.
+  const sortedLastMonthPopular = Object.fromEntries(
+    // Flips from ascending order to descending. Deconstructs object to array for sort.
+    _.reverse(Object.entries(lastMonthPopular).sort(([, a], [, b]) => a - b))
+  )
+  const sortedTotalPopular = Object.fromEntries(
+    _.reverse(Object.entries(totalPopular).sort(([, a], [, b]) => a - b))
+  )
+
+  jsonfile.writeFileSync("./data/lastMonthPopular.json", sortedLastMonthPopular)
+  jsonfile.writeFileSync("./data/totalPopular.json", sortedTotalPopular)
 })
 
 // Get fontlist keys only
